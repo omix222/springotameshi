805b653ceda38674ba643cf492b2882701a55724
Hiromi Takahashi <hiromix222@gmail.com>
2017-12-03 19:43:35 +0900

    #3 done

--- src/main/java/com/example/takahashi/springotamashi/HelloController.java
+++ src/main/java/com/example/takahashi/springotamashi/HelloController.java
@@ -7,23 +7,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.*;
 
 @Service
 @RequestMapping("/hello")
 public class HelloController {
-	
+
+	@Value("${hoge:hogehoge}")
+	String hoge;
+
 	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
-	
-	 @RequestMapping(method=RequestMethod.GET)
-	    public String hello() {
-		 Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		 String username="default";
-		 if (principal instanceof UserDetails) {
-		   username = ((UserDetails)principal).getUsername();
-		 } else {
-		   username = principal.toString();
-		 }
-		 logger.info("User : " + username);
-	        return "hellospringmvc";
-	    }
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String hello() {
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String username = "default";
+		if (principal instanceof UserDetails) {
+			username = ((UserDetails) principal).getUsername();
+		} else {
+			username = principal.toString();
+		}
+		logger.info("User : " + username);
+		logger.info("${hoge}:" + hoge);
+		return "hellospringmvc";
+	}
 }
