400290633a39b1279826d5d522f9925a3b576194
Hiromi Takahashi <hiromix222@gmail.com>
2018-01-27 12:45:41 +0900

    #23 quitできるようにfix

--- src/test/java/com/example/takahashi/springotamashi/HelloControllerTest.java
+++ src/test/java/com/example/takahashi/springotamashi/HelloControllerTest.java
@@ -77,7 +77,7 @@ public class HelloControllerTest {
  
 	@After
 	  public void tearDown() throws Exception {
-	//    driver.quit();
+	    driver.quit();
 	    String verificationErrorString = verificationErrors.toString();
 	    if (!"".equals(verificationErrorString)) {
 	      fail(verificationErrorString);
