c054511559095f86d411597fff7cb2711db54a9b
Hiromi Takahashi <hiromix222@gmail.com>
2018-02-21 00:01:22 +0900

    #29 done

---
+++ src/main/java/com/example/takahashi/springotamashi/AppConfiguration.java
@@ -0,0 +1,14 @@
+package com.example.takahashi.springotamashi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class AppConfiguration {
+	@Bean
+	  public StubLoggingFilter requestLoggingFilter() {
+		StubLoggingFilter filter = new StubLoggingFilter();
+	    return filter;
+	  }
+}
